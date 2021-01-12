cd5213ed4313006e8aeef44e6a9da7b6e6376965
tarunaygr <60793650+tarunaygr@users.noreply.github.com>
2021-01-12 20:37:59 +0530

    done

---
+++ spider.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+for i in range(1,11):
+    req=requests.get(f"https://quotes.toscrape.com/page/{i}/")
+    body=bs4.BeautifulSoup(req.text,'lxml')
+    div_container=body.find('div',attrs={"class":"container"})
+    div_row=div_container.find_all('div',attrs={"class":"row"})[1]
+    div_col=div_row.find_all('div',attrs={"class":"col-md-8"})
+    div_quote=div_col[0].find_all('div',attrs={"class":"quote"})
+    for div in div_quote:
+        text=div.find('span',{"class":"text"})
+        print(text.text)
